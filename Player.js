ec9e918e11f07287690468ea79d11efc1cc146ab
aathmika10 <65343462+aathmika10@users.noreply.github.com>
2020-07-09 20:49:42 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,23 @@
+class Player{
+    constructor(){
+
+    }
+    getCount(){
+        database.ref('playerCount').on("value",function(data){
+        playerCount=data.val();
+        })
+    }
+    updateCount(count){
+        database.ref('/').update({
+            playerCount:count
+        })
+    }
+    update(name){
+        console.log(playerCount)
+        var playerIndex= "player"+playerCount
+        console.log(playerIndex)
+        database.ref(playerIndex).set({
+            name:name
+        })
+    }
+}
